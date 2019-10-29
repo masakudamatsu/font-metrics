8f03eb1be8c38c87d84db37f53b7564e4e999b41
Unknown <m.kudamatsu@gmail.com>
2019-10-29 12:26:32 +0900

    #6 Red: Write the second test with Roboto Slab Light
    To see if uploading a different font file will display the corresponding different font name

--- cypress/integration/uploadFontFile.spec.js
+++ cypress/integration/uploadFontFile.spec.js
@@ -7,7 +7,6 @@ describe('Uploading a font file', () => {
   describe('Displays the font name', () => {
 
     it('When it is Open Sans Regular', () => {
-
       const fontFileName = 'OpenSans-Regular.ttf';
       const expectedFontName = 'Open Sans Regular';
 
@@ -24,5 +23,24 @@ describe('Uploading a font file', () => {
 
       cy.contains(expectedFontName);
     });
+
+    it('When it is Roboto Slab Light', () => {
+      const fontFileName = 'RobotoSlab-Light.ttf';
+      const expectedFontName = 'Roboto Slab Light';
+
+      cy.fixture(fontFileName, 'binary')
+        .then(Cypress.Blob.binaryStringToBlob)
+        .then((fontFile) => {
+          cy.get('[data-test=FontFileUploader]').upload({
+            fileContent: fontFile,
+            fileName: fontFileName,
+            mimeType: 'font/ttf',
+            encoding: 'utf8'
+          });
+        });
+
+      cy.contains(expectedFontName);
+    });
+
   });
 });
