08b660e3320d8b0d5c99cf905939748342621bd5
Unknown <m.kudamatsu@gmail.com>
2019-10-30 16:13:06 +0900

    #9 Green: Call the change handler upon a file upload

--- src/components/FontFileUploader.js
+++ src/components/FontFileUploader.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 export default class FontFileUploader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event) {
+    const fileList = event.target.files;
+    this.props.onChange(fileList);
+  }
   render() {
     return (
       <div>
         <input type="file"
-               data-test="FontFileUploader" />
+               data-testid="FontFileUploader"
+               onChange={this.handleChange}
+        />
       </div>
     );
   }
