e6b1c836d4911ec67bf2be0d4ce579fe002c3786
Unknown <m.kudamatsu@gmail.com>
2019-10-31 10:17:21 +0900

    #10 Green: Extract the font name from a font object

---
+++ src/helper/getFontMetrics.js
@@ -0,0 +1,15 @@
+const getFontMetrics = (font) => {
+  try {
+    const opentype = require('opentype.js');
+    if (!font.tables) {
+      throw Error('The font.tables property does not exist.')
+    }
+    const namingTable = font.tables[name];
+    const fontName = namingTable[fullName];
+    return fontName;
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+export default getFontMetrics;
