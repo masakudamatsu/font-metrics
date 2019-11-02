a4325419d5c96dd0be513d1895a227d628a2c6a6
Unknown <m.kudamatsu@gmail.com>
2019-11-02 16:46:01 +0900

    #12 Refactor: Make FontFileUploader reusable for other file types

--- src/components/FontFileUploader.js
+++ src/components/FontFileUploader.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
+const acceptableFileExtensions = ['.ttf', '.otf', 'woff'];
+const acceptableFileExtensionsInRegExp = /(\.ttf|\.otf|\.woff)$/i;
+
 export default class FontFileUploader extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
   }
   handleChange(event) {
-    const fileList = event.target.files;
-    this.props.onChange(fileList);
+    const file = event.target.files[0];
+    if (acceptableFileExtensionsInRegExp.test(file.name)) {
+      this.props.onChange(file);
+    } else { // Handle edge cases
+      this.props.onChangeWithInvalidFile();
+    }
   }
   render() {
     return (
       <div>
         <input type="file"
                data-testid="FontFileUploader"
-               accept=".otf, .ttf, .woff"
+               accept={acceptableFileExtensions.join()}
                onChange={this.handleChange}
         />
       </div>
