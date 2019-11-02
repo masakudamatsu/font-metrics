c6629178728ba96036118fffb2e21cfebe3134c6
Unknown <m.kudamatsu@gmail.com>
2019-11-02 17:16:12 +0900

    #12 Refactor: Test four file types (OTF, TTF, WOFF, and fake font file)
    To make sure that the manipulated MINE type won't pass

--- src/components/FontFileUploader.test.js
+++ src/components/FontFileUploader.test.js
@@ -10,30 +10,76 @@ describe('<FontFileUploader />', () => {
 
   afterEach(cleanup);
 
-  describe('Uploading a font file', () => {
-    let fileChangeHandler;
-    beforeEach(() => {
-      fileChangeHandler = jest.fn();
-      const {getByTestId} = render(<FontFileUploader onChange={fileChangeHandler}/>);
+  let fileChangeHandler,
+      invalidFileHandler,
+      inputElement;
+  beforeEach(() => {
+    fileChangeHandler = jest.fn();
+    invalidFileHandler = jest.fn();
+    const {getByTestId} = render(<FontFileUploader
+      onChange={fileChangeHandler}
+      onChangeWithInvalidFile={invalidFileHandler}
+    />);
 
-      const file = new File(['dummy data'], 'dummytypeface.ttf', {
+    inputElement = getByTestId('FontFileUploader');
+  });
+
+  describe('Uploading a text file with a fake MIME type', () => {
+    it('calls the file change hanlder for invalid files', () => {
+      const textFile = new File(['dummy text'], 'dummytypeface.txt', {
         type: 'font/ttf',
       });
-
-      const inputElement = getByTestId('FontFileUploader');
-
       Object.defineProperty(inputElement, 'files', {
-        value: [file],
+        value: [textFile],
       });
 
       fireEvent.change(inputElement);
 
     });
+  });
 
-    it('calls the file change handler', () => {
+  describe('Uploading a font file', () => {
+    it('calls the file change handler if it is a TTF file', () => {
+
+      const ttfFile = new File(['dummy data'], 'dummytypeface.ttf', {
+        type: 'font/ttf',
+      });
+      Object.defineProperty(inputElement, 'files', {
+        value: [ttfFile],
+      });
+
+      fireEvent.change(inputElement);
 
       expect(fileChangeHandler).toHaveBeenCalled();
-
     });
+
+    it('calls the file change handler if it is a OTF file', () => {
+
+      const otfFile = new File(['dummy data'], 'dummytypeface.otf', {
+        type: 'font/otf',
+      });
+      Object.defineProperty(inputElement, 'files', {
+        value: [otfFile],
+      });
+
+      fireEvent.change(inputElement);
+
+      expect(fileChangeHandler).toHaveBeenCalled();
+    });
+
+    it('calls the file change handler if it is a WOFF file', () => {
+
+      const woffFile = new File(['dummy data'], 'dummytypeface.woff', {
+        type: 'font/woff',
+      });
+      Object.defineProperty(inputElement, 'files', {
+        value: [woffFile],
+      });
+
+      fireEvent.change(inputElement);
+
+      expect(fileChangeHandler).toHaveBeenCalled();
+    });
+
   });
 });
