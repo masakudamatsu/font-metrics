b6d1574f0358452c888dfea74e49c7e0acbf4e07
Unknown <m.kudamatsu@gmail.com>
2019-11-02 11:04:53 +0900

    #11 Red: Write a test for FontNameDisplay component

---
+++ src/components/FontNameDisplay.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  cleanup,
+  render,
+} from '@testing-library/react';
+import FontNameDisplay from './FontNameDisplay';
+
+describe('<FontNameDisplay />', () => {
+  it('Displays this.props.fontName', () => {
+    const inputText = 'Awesome Font';
+    const expectedText = 'Awesome Font';
+
+    const {getByTestId} = render(<FontNameDisplay fontName={inputText} />);
+
+    expect(getByTestId('FontNameDisplay').textContent).toBe(expectedText);
+  });
+});
