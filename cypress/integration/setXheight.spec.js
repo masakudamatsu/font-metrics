53436f7efef60bbfdeb2144f7fd4ebce59607178
Unknown <m.kudamatsu@gmail.com>
2019-11-03 15:45:45 +0900

    #14 Red->Green: Write additional tests for mapping x-height to font-size

--- cypress/integration/setXheight.spec.js
+++ cypress/integration/setXheight.spec.js
@@ -5,11 +5,13 @@ describe('Setting x-height', () => {
     cy.visit('http://localhost:3000');
   });
 
-  describe('Displays the corresponding font-size property value for the uploaded font', () => {
+  afterEach(() => {
+    cy.get('[data-testid=XheightBox]').clear();
+  });
 
-    it('If Open Sans Regular was uploaded', () => {
+  describe('Displays the corresponding font-size property value for Open Sans Regular', () => {
+    beforeEach(() => {
       const fontFileName = 'OpenSans-Regular.ttf';
-      const expectedFontName = 'Open Sans Regular';
       cy.fixture(fontFileName, 'binary')
         .then(Cypress.Blob.binaryStringToBlob)
         .then((fontFile) => {
@@ -20,13 +22,44 @@ describe('Setting x-height', () => {
             encoding: 'utf8'
           });
         });
+    });
 
+    it('If the user chooses 16px', () => {
       cy.get('[data-testid=XheightBox]').type('16');
+      cy.get('[data-testid=FontSizeBox]').should('have.value', '29.8978');
+    });
 
-      cy.get('[data-testid=FontSizeBox]').should('have.value', '29.8978');  
+    it('If the user chooses 23px', () => {
+      cy.get('[data-testid=XheightBox]').type('23');
+      cy.get('[data-testid=FontSizeBox]').should('have.value', '42.9781');
+    });
 
-    })
-  })
+  });
 
+  describe('Displays the corresponding font-size property value for Roboto Slab Light', () => {
+    beforeEach(() => {
+      const fontFileName = 'RobotoSlab-Light.ttf';
+      cy.fixture(fontFileName, 'binary')
+        .then(Cypress.Blob.binaryStringToBlob)
+        .then((fontFile) => {
+          cy.get('[data-testid=FontFileUploader]').upload({
+            fileContent: fontFile,
+            fileName: fontFileName,
+            mimeType: 'font/ttf',
+            encoding: 'utf8'
+          });
+        });
+    });
 
+    it('If the user chooses 18px', () => {
+      cy.get('[data-testid=XheightBox]').type('18');
+      cy.get('[data-testid=FontSizeBox]').should('have.value', '34.0702');
+    });
+
+    it('If the user chooses 21px', () => {
+      cy.get('[data-testid=XheightBox]').type('21');
+      cy.get('[data-testid=FontSizeBox]').should('have.value', '39.7486');
+    });
+
+  });
 });
