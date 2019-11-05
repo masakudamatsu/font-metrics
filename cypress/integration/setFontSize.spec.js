232bb8131d47fefb98148d00a42e18b8e380ec24
Unknown <m.kudamatsu@gmail.com>
2019-11-05 17:40:43 +0900

    #17 Red->Green: Render paragraphs at the user-selected font-size
    The existing code has already implemented this feature.

--- cypress/integration/setFontSize.spec.js
+++ cypress/integration/setFontSize.spec.js
@@ -37,20 +37,20 @@ describe('Setting font-size', () => {
         cy.get('[data-testid=XheightBox]').should('have.value', expectedXHeightValue);
       });
     });
-    // describe('Renders the sample paragraph accordingly', () => {
-    //   it('if it is set to be 16px', () => {
-    //     const xHeightValue = '16';
-    //     const expectedFontSize = '29.8978px';
-    //     cy.get('[data-testid=XheightBox]').type(xHeightValue);
-    //     cy.get('[data-testid=SampleParagraph]').should('have.css', 'font-size', expectedFontSize);
-    //   });
-    //   it('if it is set to be 23px', () => {
-    //     const xHeightValue = '23';
-    //     const expectedFontSize = '42.9781px';
-    //     cy.get('[data-testid=XheightBox]').type(xHeightValue);
-    //     cy.get('[data-testid=SampleParagraph]').should('have.css', 'font-size', expectedFontSize);
-    //   });
-    // });
+    describe('Renders the sample paragraph accordingly', () => {
+      it('if it is set to be 16px', () => {
+        const fontSizeValue = '16';
+        const expectedFontSize = '16px';
+        cy.get('[data-testid=FontSizeBox]').type(fontSizeValue);
+        cy.get('[data-testid=SampleParagraph]').should('have.css', 'font-size', expectedFontSize);
+      });
+      it('if it is set to be 23px', () => {
+        const fontSizeValue = '23';
+        const expectedFontSize = '23px';
+        cy.get('[data-testid=FontSizeBox]').type(fontSizeValue);
+        cy.get('[data-testid=SampleParagraph]').should('have.css', 'font-size', expectedFontSize);
+      });
+    });
   });
 
   describe('after uploading Roboto Slab Light', () => {
@@ -81,19 +81,19 @@ describe('Setting font-size', () => {
         cy.get('[data-testid=XheightBox]').should('have.value', expectedXHeightValue);
       });
     });
-    // describe('Renders the sample paragraph accordingly', () => {
-    //   it('if it is set to be 18px', () => {
-    //     const xHeightValue = '18';
-    //     const expectedFontSize = '34.0702px';
-    //     cy.get('[data-testid=XheightBox]').type(xHeightValue);
-    //     cy.get('[data-testid=SampleParagraph]').should('have.css', 'font-size', expectedFontSize);
-    //   });
-    //   it('if it is set to be 21px', () => {
-    //     const xHeightValue = '21';
-    //     const expectedFontSize = '39.7486px';
-    //     cy.get('[data-testid=XheightBox]').type(xHeightValue);
-    //     cy.get('[data-testid=SampleParagraph]').should('have.css', 'font-size', expectedFontSize);
-    //   });
-    // });
+    describe('Renders the sample paragraph accordingly', () => {
+      it('if it is set to be 18px', () => {
+        const fontSizeValue = '18';
+        const expectedFontSize = '18px';
+        cy.get('[data-testid=FontSizeBox]').type(fontSizeValue);
+        cy.get('[data-testid=SampleParagraph]').should('have.css', 'font-size', expectedFontSize);
+      });
+      it('if it is set to be 21px', () => {
+        const fontSizeValue = '21';
+        const expectedFontSize = '21px';
+        cy.get('[data-testid=FontSizeBox]').type(fontSizeValue);
+        cy.get('[data-testid=SampleParagraph]').should('have.css', 'font-size', expectedFontSize);
+      });
+    });
   });
 });
