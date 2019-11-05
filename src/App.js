8359f48897079af5f98c4e081f9faf5c552a0226
Unknown <m.kudamatsu@gmail.com>
2019-11-05 14:25:32 +0900

    #13 Green: Render sample paragraphs in the uploaded font

--- src/App.js
+++ src/App.js
@@ -4,26 +4,17 @@ import FontFileUploader from './components/FontFileUploader';
 import XheightBox from './components/XheightBox';
 import FontSizeBox from './components/FontSizeBox';
 import FontNameDisplay from './components/FontNameDisplay';
+import SampleParagraphs from './components/SampleParagraphs';
 
 import getFontMetrics from './helper/getFontMetrics';
 const opentype = require('opentype.js');
-
-// Set up the canvas
-const canvasWidth = '800';
-const canvasHeight = '1000';
-const errorMessage = 'Your browser does not support the rendering of sample paragraphs in the uploaded font';
-
-// Set up typesetting parameters
-const sampleParagraph = 'different letter combinations, about what makes great typography great. It was beautiful, historical, artistically subtle in a way that science can’t capture, and I found it fascinating. None of this had even a hope of any practical application in my life. But 10 years later, when we were designing the first Macintosh computer, it all came back to me. And we designed it all into the Mac. It was the first computer with beautiful typography. If I had never ';
-const measure = 400;
-const lineHeight = 25;
-const fontSize = 16; // in pixels
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      fontName: '',
+      fontFamily: '',
+      fullName: '',
       sxHeight: '',
       unitsPerEm: '',
       userFontSize: '',
@@ -36,7 +27,7 @@ class App extends React.Component {
 
   invalidFileHandler() {
     this.setState({
-      fontName: 'Please upload an OpenType Font (.otf), TrueType Font (.ttf), or Web Open Font Format (.woff) file.'
+      fullName: 'Please upload an OpenType Font (.otf), TrueType Font (.ttf), or Web Open Font Format (.woff) file.'
     });
   }
 
@@ -48,34 +39,18 @@ class App extends React.Component {
       // Save font metrics as the state object
       const fontMetrics = getFontMetrics(font);
       this.setState({
-        fontName: fontMetrics.fontName,
+        fontFamily: fontMetrics.fontFamily,
+        fullName: fontMetrics.fullName,
         sxHeight: fontMetrics.sxHeight,
         unitsPerEm: fontMetrics.unitsPerEm
       });
-      // Render paragraphs
-      const canvas = this.refs.canvas;
-      const ctx = canvas.getContext('2d');
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      const x = 60;
-      let y = 60;
-      // Enable word wrap
-      const words = sampleParagraph.split(' ');
-      let line = '';
-      let testLine, metrics, testWidth;
-      for(let n = 0; n < words.length; n++) {
-        testLine = line + words[n] + ' ';
-        metrics = ctx.measureText(testLine);
-        testWidth = metrics.width;
-        if (testWidth > measure && n > 0) {
-          font.draw(ctx, line, x, y, fontSize);
-          line = words[n] + ' ';
-          y += lineHeight;
-        }
-        else {
-          line = testLine;
-        }
-      }
-      font.draw(ctx, line, x, y, fontSize);
+      // Load the uploaded font
+      const newFontFace = new FontFace(this.state.fontFamily, e.target.result);
+      newFontFace.load().then((loaded_face) => {
+        document.fonts.add(loaded_face);
+      }).catch((error) => {
+        console.log('The uploaded font has failed to be loaded,');
+      });
     }).bind(this);
     reader.readAsArrayBuffer(fontFile);
   }
@@ -99,13 +74,8 @@ class App extends React.Component {
         <FontSizeBox
           fontSize={this.state.userFontSize}
         />
-        <FontNameDisplay fontName={this.state.fontName} />
-        <canvas
-                ref="canvas"
-                height={canvasHeight}
-                width={canvasWidth} >
-          {errorMessage}
-        </canvas>
+        <FontNameDisplay fontName={this.state.fullName} />
+        <SampleParagraphs fontFamily={this.state.fontFamily}/>
       </div>
     );
   }
