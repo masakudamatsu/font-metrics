7e27708efbdff8b9bfa0af57caee30df0b002c09
Unknown <m.kudamatsu@gmail.com>
2019-11-05 16:45:54 +0900

    #15 Red: Write a test for rendering paragraphs at the chosen x-height

--- cypress/integration/setXheight.spec.js
+++ cypress/integration/setXheight.spec.js
@@ -9,7 +9,7 @@ describe('Setting x-height', () => {
     cy.get('[data-testid=XheightBox]').clear();
   });
 
-  describe('for Open Sans Regular', () => {
+  describe('after uploading Open Sans Regular', () => {
     beforeEach(() => {
       const fontFileName = 'OpenSans-Regular.ttf';
       cy.fixture(fontFileName, 'binary')
@@ -25,17 +25,35 @@ describe('Setting x-height', () => {
     });
     describe('Displays the corresponding font-size property value', () => {
       it('if it is set to be 16px', () => {
-        cy.get('[data-testid=XheightBox]').type('16');
-        cy.get('[data-testid=FontSizeBox]').should('have.value', '29.8978');
+        const xHeightValue = '16';
+        const expectedFontSizeValue = '29.8978';
+        cy.get('[data-testid=XheightBox]').type(xHeightValue);
+        cy.get('[data-testid=FontSizeBox]').should('have.value', expectedFontSizeValue);
       });
       it('if it is set to be 23px', () => {
-        cy.get('[data-testid=XheightBox]').type('23');
-        cy.get('[data-testid=FontSizeBox]').should('have.value', '42.9781');
+        const xHeightValue = '23';
+        const expectedFontSizeValue = '42.9781';
+        cy.get('[data-testid=XheightBox]').type(xHeightValue);
+        cy.get('[data-testid=FontSizeBox]').should('have.value', expectedFontSizeValue);
+      });
+    });
+    describe('Renders the sample paragraph accordingly', () => {
+      it('if it is set to be 16px', () => {
+        const xHeightValue = '16';
+        const expectedFontSize = '29.8978px';
+        cy.get('[data-testid=XheightBox]').type(xHeightValue);
+        cy.get('[data-testid=SampleParagraph]').should('have.css', 'font-size', expectedFontSize);
+      });
+      it('if it is set to be 23px', () => {
+        const xHeightValue = '23';
+        const expectedFontSize = '42.9781px';
+        cy.get('[data-testid=XheightBox]').type(xHeightValue);
+        cy.get('[data-testid=SampleParagraph]').should('have.css', 'font-size', expectedFontSize);
       });
     });
   });
 
-  describe('for Roboto Slab Light', () => {
+  describe('after uploading Roboto Slab Light', () => {
     beforeEach(() => {
       const fontFileName = 'RobotoSlab-Light.ttf';
       cy.fixture(fontFileName, 'binary')
@@ -51,13 +69,32 @@ describe('Setting x-height', () => {
     });
     describe('Displays the corresponding font-size property value', () => {
       it('if it is set to be 18px', () => {
-        cy.get('[data-testid=XheightBox]').type('18');
-        cy.get('[data-testid=FontSizeBox]').should('have.value', '34.0702');
+        const xHeightValue = '18';
+        const expectedFontSizeValue = '34.0702';
+        cy.get('[data-testid=XheightBox]').type(xHeightValue);
+        cy.get('[data-testid=FontSizeBox]').should('have.value', expectedFontSizeValue);
       });
       it('if it is set to be 21px', () => {
-        cy.get('[data-testid=XheightBox]').type('21');
-        cy.get('[data-testid=FontSizeBox]').should('have.value', '39.7486');
+        const xHeightValue = '21';
+        const expectedFontSizeValue = '39.7486';
+        cy.get('[data-testid=XheightBox]').type(xHeightValue);
+        cy.get('[data-testid=FontSizeBox]').should('have.value', expectedFontSizeValue);
       });
     });
+    describe('Renders the sample paragraph accordingly', () => {
+      it('if it is set to be 18px', () => {
+        const xHeightValue = '18';
+        const expectedFontSize = '34.0702px';
+        cy.get('[data-testid=XheightBox]').type(xHeightValue);
+        cy.get('[data-testid=SampleParagraph]').should('have.css', 'font-size', expectedFontSize);
+      });
+      it('if it is set to be 21px', () => {
+        const xHeightValue = '21';
+        const expectedFontSize = '39.7486px';
+        cy.get('[data-testid=XheightBox]').type(xHeightValue);
+        cy.get('[data-testid=SampleParagraph]').should('have.css', 'font-size', expectedFontSize);
+      });
+    });
+
   });
 });
