879c8d109f4f11e0ea7d62e15f040c84f4ad3c21
Unknown <m.kudamatsu@gmail.com>
2019-11-07 10:49:20 +0900

    #18 Red: Write a test for mapping the modular scale to line-height value

---
+++ cypress/integration/setModularScale.spec.js
@@ -0,0 +1,58 @@
+describe('The modular scale', () => {
+
+  before(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  afterEach(() => {
+  });
+
+  describe('with Open Sans Regular at 16px', () => {
+    beforeEach(() => {
+      const fontFileName = 'OpenSans-Regular.ttf';
+      const fontSizeValue = '16';
+
+      cy.fixture(fontFileName, 'binary')
+        .then(Cypress.Blob.binaryStringToBlob)
+        .then((fontFile) => {
+          cy.get('[data-testid=FontFileUploader]').upload({
+            fileContent: fontFile,
+            fileName: fontFileName,
+            mimeType: 'font/ttf',
+            encoding: 'utf8'
+          });
+        });
+      cy.get('[data-testid=FontSizeBox]').type(fontSizeValue);
+    });
+    afterEach(() => {
+      cy.get('[data-testid=ScaleBoxX]').clear();
+      cy.get('[data-testid=ScaleBoxLine]').clear();
+    });
+    describe('Choosing the x-height to line-height ratio to be 1:3', () => {
+      beforeEach(() => {
+        const xHeightScale = 1;
+        const lineHeightScale = 3;
+        cy.get('[data-testid=ScaleBoxX]').type(xHeightScale);
+        cy.get('[data-testid=ScaleBoxLine]').type(lineHeightScale);
+      });
+      it('Sets the line-height property value to be 1.6055', () => {
+        const expectedLineHeightValue = '1.6055';
+        cy.get('[data-testid=LineHeightBox]').should('have.value', expectedLineHeightValue);
+      });
+    })
+    describe('Choosing the x-height to line-height ratio to be 2:5', () => {
+      beforeEach(() => {
+        const xHeightScale = 2;
+        const lineHeightScale = 5;
+        cy.get('[data-testid=ScaleBoxX]').type(xHeightScale);
+        cy.get('[data-testid=ScaleBoxLine]').type(lineHeightScale);
+      });
+      it('Sets the line-height property value to be 1.3379', () => {
+        const expectedLineHeightValue = '1.3379';
+        cy.get('[data-testid=LineHeightBox]').should('have.value', expectedLineHeightValue);
+      });
+    });
+
+  });
+
+});
